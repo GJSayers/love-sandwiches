90ea5c6358f745cb46ad0a37fe8090d5817ff6f2
GJSayers <gemma.sayers1@btinternet.com>
2021-05-23 15:53:49 +0000

    add while lopp to improve ux and return data

--- run.py
+++ run.py
@@ -16,14 +16,19 @@ def get_sales_data():
     """
     Get sales figures input from the user
     """
-    print("Please enter sales data from the last market.")
-    print("Data should be six numbers, separated by commas.")
-    print("Example: 10,20,30,40,50,60\n")
-    data_str = input("Enter your data here: ")
-    print(f"The data provided is {data_str}")
+    while True:
+        print("Please enter sales data from the last market.")
+        print("Data should be six numbers, separated by commas.")
+        print("Example: 10,20,30,40,50,60\n")
+        data_str = input("Enter your data here: ")
+        print(f"The data provided is {data_str}")
 
-    sales_data = data_str.split(",")
-    validate_data(sales_data)
+        sales_data = data_str.split(",")
+        
+        if validate_data(sales_data):
+            print("Data is vaild!")
+            break
+    return sales_data    
 
 def validate_data(values):
     """ 
@@ -31,13 +36,19 @@ def validate_data(values):
     Raises ValueError if strings cannot be converted to int,
     or if ther are not 6 values exactly
     """
+    print(values)
     try:
+        [int(value) for value in values]
         if len(values) != 6:
             raise ValueError(
         f"Exactly 6 values required, you provided {len(values)}"
         )  
     except ValueError as e:
-        print(f"Invalid data: {e}, please try again.\n")    
+        print(f"Invalid data: {e}, please try again.\n")  
+        return False
 
-get_sales_data()
+    return True      
+
+
+data = get_sales_data()
 
