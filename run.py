2e406df347c695dc28f8fa194182880f83a97761
GJSayers <gemma.sayers1@btinternet.com>
2021-05-23 19:28:44 +0000

    collect sales data for last 5 entirs of each type of sandwich

--- run.py
+++ run.py
@@ -82,7 +82,20 @@ def calculate_surplus_data(sales_row):
     return surplus_data
 
 
+def get_last_5_entires_sales():
+    """
+    collects columns of data from sales worksheet, collecting 
+    the last 5 entries and returns data as a list of lists
+    """
 
+    sales = SHEET.worksheet("sales")
+
+    columns = []
+    for ind in range(1,7):
+        column = sales.col_values(ind)
+        columns.append(column[-5:])
+
+    return columns
 
 
 def main():
@@ -97,4 +110,5 @@ def main():
     update_worksheet(new_surplus_data, "surplus")
 
 print("Welcome to love sandwiches data automation")
-main()
+# main()
+sales_columns = get_last_5_entires_sales()
